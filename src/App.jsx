9fe4dbd1b1003fdc3714998d5956d3b037640006
Dhirajkumar122 <105111062+Dhirajkumar122@users.noreply.github.com>
2024-03-06 19:03:13 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Board from './TicTacToeGame/Board'
+
+function App() {
+ 
+  return (
+    <>
+    <h1>Tic-Tac-toe</h1>
+    <Board/>
+      
+    </>
+  )
+}
+
+export default App
